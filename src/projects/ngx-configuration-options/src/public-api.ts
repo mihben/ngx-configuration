100bf3c343be117724e4e240ff94928b3646d611
Mihucza, Bence <mihben@gmail.com>
2025-02-07 08:18:41 +0100

    Used ngx-configuration-core package in options

--- src/projects/ngx-configuration-options/src/public-api.ts
+++ src/projects/ngx-configuration-options/src/public-api.ts
@@ -3,3 +3,11 @@
  */
 
 export { provideOptions } from './provideOptions';
+
+export { format, baseAddress } from './lib/decorators/format-decorator';
+export { range } from './lib/decorators/range-decorator';
+export { required } from './lib/decorators/required-decorator';
+export { valueOf, valueOfEnum } from './lib/decorators/value-of-decorator';
+
+export { ValidationResult } from './lib/validation-result';
+export { InvalidConfigurationError } from './lib/invalid-configuration-error';
